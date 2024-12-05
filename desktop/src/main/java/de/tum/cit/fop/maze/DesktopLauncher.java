373f447cad1ee34456663b3e764c2f737a6a3264
Eriks Spaks <erikspaks@gmail.com>
2024-12-05 18:28:11 +0100

    Added temporary files for testing character movement

--- desktop/src/main/java/de/tum/cit/fop/maze/DesktopLauncher.java
+++ desktop/src/main/java/de/tum/cit/fop/maze/DesktopLauncher.java
@@ -2,6 +2,7 @@ package de.tum.cit.fop.maze;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import de.tum.cit.fop.maze.temporary.MazeRunnerGame;
 
 /** Launches the desktop (LWJGL3) application. */
 public class DesktopLauncher {
@@ -11,7 +12,8 @@ public class DesktopLauncher {
     }
 
     private static Lwjgl3Application createApplication() {
-        return new Lwjgl3Application(new Main(), getDefaultConfiguration());
+//        return new Lwjgl3Application(new Main(), getDefaultConfiguration());
+        return  new Lwjgl3Application(new MazeRunnerGame(), getDefaultConfiguration());
     }
 
     private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
@@ -26,7 +28,7 @@ public class DesktopLauncher {
         //// If you remove the above line and set Vsync to false, you can get unlimited FPS, which can be
         //// useful for testing performance, but can also be very stressful to some hardware.
         //// You may also need to configure GPU drivers to fully disable Vsync; this can cause screen tearing.
-        configuration.setWindowedMode(640, 480);
+        configuration.setWindowedMode(1280, 960);
         //// You can change these files; they are in lwjgl3/src/main/resources/ .
         configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
         return configuration;
