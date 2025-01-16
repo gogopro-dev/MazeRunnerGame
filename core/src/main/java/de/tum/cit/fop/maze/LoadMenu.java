17106c88e94c208493e4de0defbbc07a1383aa22
Eriks Spaks <erikspaks@gmail.com>
2025-01-16 22:57:50 +0100

    Remade LoadMenu class to match singleton pattern to set screens from other classes

--- core/src/main/java/de/tum/cit/fop/maze/LoadMenu.java
+++ core/src/main/java/de/tum/cit/fop/maze/LoadMenu.java
@@ -4,13 +4,24 @@ import com.badlogic.gdx.Game;
 import de.tum.cit.fop.maze.level.LevelScreen;
 import de.tum.cit.fop.maze.menu.Menu;
 
+import java.awt.event.WindowStateListener;
+
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class LoadMenu extends Game {
+    private static LoadMenu instance;
+
     @Override
     public void create() {
         Menu menu = Menu.getInstance();
         /// Initialize the LevelScreen singleton
         new LevelScreen();
         setScreen(menu);
+    }
+
+    public static LoadMenu getInstance() {
+        if (instance == null) {
+            instance = new LoadMenu();
+        }
+        return instance;
     }
 }
